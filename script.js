8521b4af2e531ff218a8c6bfefa26c6a17cba004
R10666 <55814052+R10666@users.noreply.github.com>
2025-09-20 22:25:16 -0700

    testing js

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ const yearSpan = document.getElementById("year");
 if (yearSpan) {
     const startYear = 2024;
     const currentYear = new Date().getFullYear();
-    yearSpan.textContent = currentYear > startYear 
-        ? `${startYear}–${currentYear}` 
-        : `${startYear}`;
+
+    if (currentYear > startYear) {
+        yearSpan.textContent = `${startYear}–${currentYear}`;
+    } else {
+        yearSpan.textContent = `${startYear}`;
+    }
 }
